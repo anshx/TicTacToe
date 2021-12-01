9e5b0d682ea7cc267b97535770b6c020fc64dd20
Anshuman Panda <anshuman.panda97@gmail.com>
2021-12-01 11:59:29 +0530

    made changes to random select move

--- src/com/games/strategy/SimplePlayingStrategy.java
+++ src/com/games/strategy/SimplePlayingStrategy.java
@@ -12,9 +12,9 @@ public class SimplePlayingStrategy implements IPlayingStrategy {
     public Move play(Game game) {
         Random random = new Random();
         Move move = null;
-        int i = 1;
+        int i = 0;
         for(Move avaMoves: game.getAvailableMoves()) {
-           if(random.nextInt(i) == 0) {
+           if(random.nextInt(++i) == 0) {
                move = avaMoves;
            }
         }
